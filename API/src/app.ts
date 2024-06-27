26f192e4ca160bda0d34d358c9edc0d02632aa78
juan-larro <164416059+juan-larro@users.noreply.github.com>
2024-06-27 20:15:38 -0300

    Update app.ts

--- API/src/app.ts
+++ API/src/app.ts
@@ -1,12 +1,14 @@
 import express from 'express'
 import { clientRouter } from './Client/client.routes.js'
 import { medicalHistoryRouter } from './medicalHistory/medicalHistory.routes.js'
+import { professionalRouter } from './professional/professional.routes.js'
 
 const app = express()
 app.use(express.json())
 
 app.use('/api/clients', clientRouter)
 app.use('/api/medicalHistory', medicalHistoryRouter)
+app.use('/api/professionals', professionalRouter)
 
 app.use((_, res) => {
   return res.status(404).send({ message: 'Resource not found' })
