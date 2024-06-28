6305071fe8f4690ceb8f58a26970c7029950dc30
David1889 <115087133+David1889@users.noreply.github.com>
2024-06-28 19:40:24 -0300

    Cambio de tipo de dato de "vaccines"

--- API/src/medicalHistory/medicalHistory.entity.ts
+++ API/src/medicalHistory/medicalHistory.entity.ts
@@ -1,9 +1,10 @@
+import { RowDataPacket } from 'mysql2';
 import crypto from 'node:crypto' 
 
 export class MedicalHistory{
   constructor(
     public petId:number,
-    public vaccines:number[],
+    public vaccines:RowDataPacket[], //En un futuro se importa la clase Pet y se asigna dicho tipo de dato
     public id?: number
   ) {}
 }
