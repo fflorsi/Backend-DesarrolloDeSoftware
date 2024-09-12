15f1b1a77bf1f6517b218d900432719428910ef7
David1889 <115087133+David1889@users.noreply.github.com>
2024-09-12 12:42:59 -0300

    Update medicalHistory.entity.ts

--- API/src/medicalHistory/medicalHistory.entity.ts
+++ API/src/medicalHistory/medicalHistory.entity.ts
@@ -1,10 +1,9 @@
-import { RowDataPacket } from 'mysql2';
 import crypto from 'node:crypto' 
-
 export class MedicalHistory{
   constructor(
     public petId:number,
-    public vaccines:RowDataPacket[], //En un futuro se importa la clase Pet y se asigna dicho tipo de dato
+    public vaccines:number[],
+    public observations: number[],
     public id?: number
   ) {}
 }
