d4be52cca45fab3dc2b025f58b3515d1bd49c403
David1889 <115087133+David1889@users.noreply.github.com>
2024-09-24 11:55:46 -0300

    Update medicalHistory.entity.ts

--- API/src/medicalHistory/medicalHistory.entity.ts
+++ API/src/medicalHistory/medicalHistory.entity.ts
@@ -1,9 +1,11 @@
 import crypto from 'node:crypto' 
+import { observation } from '../observation/observations.entity';
+import { QueryResult } from 'mysql2';
 export class MedicalHistory{
   constructor(
     public petId:number,
-    public vaccines:number[],
-    public observations: number[],
+    public vaccines: any[], //Cuando esté el crud de vaccines, actualizo el tipo
+    public observations: any[],
     public id?: number
   ) {}
 }
