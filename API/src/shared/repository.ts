d54bcdf2331be2f9eb29cb007d2eaeee40da88d2
David1889 <115087133+David1889@users.noreply.github.com>
2024-09-24 12:24:13 -0300

    Delete API directory

--- API/src/shared/repository.ts
+++
@@ -1,7 +0,0 @@
-export interface Repository<T> {
-  findAll(): Promise<T[] | undefined>
-  findOne(vaccine: {id: string}): Promise<T | undefined>
-  add(vaccine: T): Promise<T | undefined>
-  update(id: string, vaccine: T): Promise<T | undefined>
-  delete(vaccine: {id:string}): Promise<T | undefined>
-}
