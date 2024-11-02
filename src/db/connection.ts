7a2d9f556357ff1bbbe5054a05e178998114c388
David <davidcanallon08@gmail.com>
2024-11-02 12:24:55 -0300

    Added cloud database

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,9 +1,10 @@
 import { Sequelize } from "sequelize";
 
 
-const sequelize = new Sequelize('veterinary', 'root', 'facundo9', {
-    host: 'localhost',
-    dialect: 'mysql',   
+const sequelize = new Sequelize('veterinary', 'avnadmin', 'AVNS_EazmPwvXfgYJy-zzvSW', {
+    host: 'mysql-veterinary-veterinary.j.aivencloud.com',
+    dialect: 'mysql',
+    port: 16432
 });
 
 export default sequelize;
